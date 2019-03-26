99f0976d087ee2923dd17d7eb160a9c0f44b483c
MDJ2014 <rolltide.mark.jones@gmail.com>
2019-03-26 11:50:16 -0400

    changed endpoint

--- app.js
+++ app.js
@@ -6,17 +6,40 @@ app.use('/public',express.static(__dirname+'/public'));
 
 
 
-
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-
-
-
-
-
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
   });
+
+
+    app.get('/:dateVal',function(req,res){
+
+        var param = decodeURIComponent(req.params.dateVal);
+        var dateOptions = {month : 'long' , year: 'numeric' , day: 'numeric'};
+
+
+
+        if(isNaN(param)){
+            if(String(new Date(param)) === "Invalid Date"){
+              res.json({unix:null, natural:null}); 
+            }
+            var naturalDate = new Date(param);
+            naturalDate = naturalDate.toLocaleDateString('en-US', dateOptions);
+            var unixDate = new Date(param).getTime()/1000;
+        }else{
+            var unixDate = Number(param)
+      var naturalDate = new Date(unixDate*1000);
+      naturalDate = naturalDate.toLocaleDateString('en-US', dateOptions);
+        }
+
+
+        res.json({unix:unixDate, natural:naturalDate}); 
+
+
+
+});
+
+
+
 
 
 
